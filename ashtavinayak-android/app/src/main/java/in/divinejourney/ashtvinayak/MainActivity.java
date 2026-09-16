899fb0951101304585e46e6f7540eacc23ca6d26
DEEPAK TIWARI <deepaktiwarisitapur@gmail.com>
2026-09-16 06:16:45 +0530

    Add offline game activity

---
+++ ashtavinayak-android/app/src/main/java/in/divinejourney/ashtvinayak/MainActivity.java
@@ -0,0 +1,4 @@
+package in.divinejourney.ashtvinayak;
+import android.app.Activity;import android.os.Bundle;import android.view.View;import android.webkit.WebChromeClient;import android.webkit.WebSettings;import android.webkit.WebView;import android.webkit.WebViewClient;import android.graphics.Color;import android.content.pm.ActivityInfo;
+public class MainActivity{
+}
